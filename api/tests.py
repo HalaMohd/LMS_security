c3c55e13c12c8b5356d1f3ec202d69392b906912
HalaMohd <hala.mohdm@hotmail.com>
2026-03-23 08:04:29 +0200

    Fix failing test

--- api/tests.py
+++ api/tests.py
@@ -3,6 +3,11 @@ from django.test import TestCase
 # Create your tests here.
 # CI pipeline test comment
 
-class DummyFailTest(TestCase):
+""" class DummyFailTest(TestCase):
     def test_fail_on_purpose(self):
         self.assertEqual(1, 2)
+ """
+
+class DummyPassTest(TestCase):
+    def test_pass(self):
+        self.assertEqual(1, 1)

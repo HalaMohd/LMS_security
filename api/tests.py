a13945ebdc3f6c5ee3974779d552c0e7711651c3
HalaMohd <hala.mohdm@hotmail.com>
2026-03-23 08:00:06 +0200

    Add failing test for CI check

--- api/tests.py
+++ api/tests.py
@@ -2,3 +2,7 @@ from django.test import TestCase
 
 # Create your tests here.
 # CI pipeline test comment
+
+class DummyFailTest(TestCase):
+    def test_fail_on_purpose(self):
+        self.assertEqual(1, 2)
